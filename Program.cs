d65e05d3e828018652617109ab85ed8f2b6ffef2
Guilherme Gentile <uk.gentile@gmail.com>
2018-09-29 11:10:00 -0300

    Inicial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fiap01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+        }
+    }
+}
+
+public class Startup 
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env) 
+    {
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
