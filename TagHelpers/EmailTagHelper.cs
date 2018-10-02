339a009419f3300ce26343557b8e6481775d4331
Guilherme <uk.gentile@gmail.com>
2018-10-01 22:33:37 -0300

    ;

---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap01.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string MailTo { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var address = $"{MailTo}@fiap.com";
+
+            output.Attributes.SetAttribute("href", $"mailto:{address}");
+            output.Content.SetContent(address);
+        }
+    }
+}
---
+++ ViewComponents/NoticiasViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap01.ViewComponents
+{
+    public class NoticiasViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int total, bool noticiasUrgentes)
+        {
+            string view = "noticias";
+
+            if (total > 3 && noticiasUrgentes == true)
+            {
+                view = "noticiasurgentes";
+            }
+
+            var itens = GetItens(total);
+            return View(view, itens);
+        }
+
+        private IEnumerable<Noticia> GetItens(int total)
+        {
+            for (int i = 0; i < total; i++)
+            {
+                yield return new Noticia() { Id = 1, Titulo = $"Noticia {i}", Link = $"http://{i}" };
+            }
+        }
+    }
+
+    public class Noticia
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Link { get; set; }
+    }
+}
